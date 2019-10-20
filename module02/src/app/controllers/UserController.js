12c236853148800d6883fa52841dc59ed59db5c6
Joao Marques <joao@jjmf.com>
2019-10-20 20:25:09 -0300

    Auth middleware, data validation and user update.

--- module02/src/app/controllers/UserController.js
+++ module02/src/app/controllers/UserController.js
@@ -1,8 +1,23 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    // Check it the user already exists by search for the user's email.
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    // Check if the user already exists by searching for the user's email.
     const userExists = await User.findOne({ where: { email: req.body.email } });
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
@@ -11,6 +26,51 @@ class UserController {
     // User doesn't exists. Create it.
     // const user = await User.create(req.body); // return full user data
     const { id, name, email, provider } = await User.create(req.body); // return only some data
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    // Check if the user already exists by searching for the user's email.
+    const user = await User.findByPk(req.userId);
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    // Check actual password.
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Actual password does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
 
     return res.json({
       id,
--- module02/src/routes.js
+++ module02/src/routes.js
@@ -4,9 +4,16 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
+// Middlewares.
+import authMiddleware from './app/middlewares/auth';
+
 // Routes.
 const routes = new Router();
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+
+// The following routes use the authMiddleware.
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
