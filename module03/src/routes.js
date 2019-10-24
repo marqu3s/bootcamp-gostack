63c27672a05d5c84f838fc2aa3e79ba86fdd6d8c
Joao Marques <joao@jjmf.com>
2019-10-23 23:05:42 -0300

    Schedule list.

--- module03/src/routes.js
+++ module03/src/routes.js
@@ -8,6 +8,7 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 // Middlewares.
 import authMiddleware from './app/middlewares/auth';
@@ -29,5 +30,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 // Appointments routes.
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
