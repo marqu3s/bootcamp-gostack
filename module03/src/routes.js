9dfb2740cdbf27da34bf25a93aed6f02223ded29
Joao Marques <joao@jjmf.com>
2019-10-25 21:59:36 -0300

    Read notification, canceling appointments.

--- module03/src/routes.js
+++ module03/src/routes.js
@@ -3,12 +3,13 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 // Import controllers here.
+import AppointmentController from './app/controllers/AppointmentController';
+import FileController from './app/controllers/FileController';
+import NotificationController from './app/controllers/NotificationController';
+import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
-import FileController from './app/controllers/FileController';
-import ProviderController from './app/controllers/ProviderController';
-import AppointmentController from './app/controllers/AppointmentController';
-import ScheduleController from './app/controllers/ScheduleController';
 
 // Middlewares.
 import authMiddleware from './app/middlewares/auth';
@@ -30,6 +31,11 @@ routes.post('/files', upload.single('file'), FileController.store);
 // Appointments routes.
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 routes.get('/schedules', ScheduleController.index);
+
+// Notifications routes.
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
