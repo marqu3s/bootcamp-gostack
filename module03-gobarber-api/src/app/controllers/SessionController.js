ed0621ca300777ecee7228fb0c10ee8001665e54
Joao Marques <joao@jjmf.com>
2019-11-10 00:24:15 -0300

    GoBarber API adjustments and begining of frontend app.

--- module03-gobarber-api/src/app/controllers/SessionController.js
+++ module03-gobarber-api/src/app/controllers/SessionController.js
@@ -1,12 +1,34 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } }); // email: email
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    }); // email: email
 
     // Verify that user exists.
     if (!user) {
@@ -18,11 +40,15 @@ class SessionController {
       res.status(401).json({ error: 'Password does not match.' });
     }
 
+    const { id, name, avatar, provider } = user;
+
     return res.json({
       user: {
-        id: user.id,
-        name: user.name,
+        id,
+        name,
         email,
+        provider,
+        avatar,
       },
       token: jwt.sign({ id: user.id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
