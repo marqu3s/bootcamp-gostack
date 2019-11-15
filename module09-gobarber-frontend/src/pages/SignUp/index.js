3c87530411979ad2115d787e8d805c0804ca7829
Joao Marques <joao@jjmf.com>
2019-11-14 22:35:20 -0300

    Redux config

--- module09-gobarber-frontend/src/pages/SignUp/index.js
+++ module09-gobarber-frontend/src/pages/SignUp/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('Campo obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('Campo obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve ter no mínimo 6 caracteres')
+    .required('Campo obrigatório'),
+});
+
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input placeholder="Seu nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Seu nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
